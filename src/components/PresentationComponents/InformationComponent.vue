73e66ee9f9fdf6d827c7023507f99e94205d810b
Thirty33 <joelsuarez.1101@gmail.com>
2019-05-23 18:35:14 -0500

    UserProperties component structure finished

---
+++ src/components/PresentationComponents/InformationComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="information_wrapper">
+			<div id="information_title_container">
+				<h2 id="information_title">{{ itemInformation.title_information }}</h2>
+			</div>
+			<div id="information_items_container">
+				<h4 id="information_item" class="item1"
+					v-for="item_information in itemInformation.items_information"
+					:key="item_information.text">
+						{{ item_information.text }}
+				</h4>
+			</div>
+		</div>
+</template>
+
+<script>
+export default {
+	props: ['itemInformation'],
+	data() {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style scoped>
+#information_wrapper {
+	background-color: aqua;
+	display: grid;
+	grid-template-rows: 1fr 3fr;
+	grid-gap: 0;
+}
+
+#information_title_container {
+	background-color: purple;
+	opacity: 0.3;
+	grid-row: 1;
+	font-size: 1.2em;
+}
+
+#information_items_container {
+	background-color: brown;
+	opacity: 0.3;
+	grid-row: 2;
+	
+	display: grid;
+	grid-gap: 0;
+	grid-template-rows: repeat(3, 1fr);
+	grid-template-columns: 1fr 1fr;
+	font-size: 1.2em;
+}
+
+#information_title_container,
+#information_items_container {
+	text-align: center;
+}
+</style>
+
+
--- src/components/UserProperties.vue
+++ src/components/UserProperties.vue
@@ -1,16 +1,77 @@
 <template>
-	<div class="">
-		User information component
+	<div id="user_information_main_container">
+		<div id="user_information_wrapper">
+			<div id="user_information_container">
+				<information-component
+				v-for="itemInformation in texts_information"
+				:key=itemInformation.title_information
+				:itemInformation="itemInformation">
+				</information-component>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import informationComponent from './PresentationComponents/InformationComponent.vue'
 export default {
-    
+	data() {
+		return {
+			texts_information: [
+				{ title_information: 'Estudios', items_information: [
+					{ text: '5to semestre Ing. (ULA VE)' },
+					{ text: 'Desarrollo web (Platzi)' },
+					{ text: 'Redes Cisco (Cisco Academy)' }
+				]},
+				{ title_information: 'Conocimientos', items_information: [
+					{ text: 'Maquetacion (html, css, js)' },
+					{ text: 'Arquitectura MVC, MVVM' },
+					{ text: 'API REST' },
+					{ text: 'MYSQL' },
+					{ text: 'POSTGRESQL' },
+				]},
+				{ title_information: 'Habilidades adicionales', items_information: [
+					{ text: 'Diseno servicios (SWAGGER)' },
+					{ text: 'Organizacion de tareas (TRELLO)' },
+					{ text: 'Manejo de tiempo (Pomodoro technique)' }
+				]},
+				{ title_information: 'Lenguajes', items_information: [
+					{ text: 'Javascript' },
+					{ text: 'C#' },
+					{ text: 'PHP' },
+					{ text: 'C++' }
+				]},
+				{ title_information: 'Frameworks', items_information: [
+					{ text: 'Vue.js' },
+					{ text: 'Xamarin Forms' },
+					{ text: '.Net Core' }
+				]}
+			]
+		}
+	},
+	components: {
+		informationComponent
+	}
 }
 </script>
 
-<style>
+<style scoped>
+#user_information_wrapper {
+	color: white;
+	background-color: purple;
+	height: inherit;
+	position: relative;
+	overflow-y:scroll;
+}
+
+#user_information_container {
+	background-color: gray;
+	position: absolute;
+	right: 0.7em;
+  left: 0.7em;
+	top: 2em;
+	
+}
 
 </style>
 
